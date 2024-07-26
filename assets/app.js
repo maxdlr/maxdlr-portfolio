078136f63362325ff2aaec12a15eb056541c61de
maxdlr <contact@maxdlr.com>
2024-07-26 12:37:48 +0200

    setup dependencies and sociallinks

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+import { registerVueControllerComponents } from '@symfony/ux-vue';
 import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
@@ -5,6 +6,8 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
---
+++ assets/vue/component/SocialLink.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  link: {type: String, required: true},
+  icon: {type: String, required: true}
+})
+</script>
+
+<template>
+  <a
+      :href="link"
+      class="link-underline link-underline-opacity-0 text-white fs-1"
+      target="_blank"
+  >
+    <i
+        class="bi"
+        :class="`bi-${icon}`"
+    ></i>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
