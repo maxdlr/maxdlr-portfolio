cc3f1ed9d66d1a3254dd967930b1b071363b46e9
maxdlr <contact@maxdlr.com>
2025-01-28 16:37:29 +0100

    fix cors

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
         secure: true,
       },
     },
+    cors: {
+      origin: ["https://docs.maxdlr.com", "https://www.maxdlr.com"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    },
   },
 });
