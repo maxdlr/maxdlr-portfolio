892e8d5702ebcc96aa5accafb805f2116f40da5b
maxdlr <contact@maxdlr.com>
2025-02-04 22:34:27 +0100

    refactor: move text processing logic into new class && add animation on load

--- src/components/pages/Blog/BlogArticle.vue
+++ src/components/pages/Blog/BlogArticle.vue
@@ -5,19 +5,19 @@ import { BlogService } from "../../../services/BlogService.ts";
 import router from "../../../router";
 import { usePageHead } from "../../../composables/usePageHead.ts";
 import { formatDate } from "@vueuse/core";
-import { marked } from "marked";
 import Loader from "../../atoms/Loader.vue";
-import { Utils } from "../../../composables/Utils.ts";
 import { ImgProcessor } from "../../../composables/ImgProcessor.ts";
+import { BlogArticleTextProcessor } from "../../../composables/BlogArticleTextProcessor.ts";
+import TransitionSlideUp from "../../atoms/TransitionSlideUp.vue";
 
 const article: Ref<BlogArticle> = ref({} as BlogArticle);
 const id: Ref<string | null> = ref(null);
 const isLoading = ref(false);
 const imageProcessor = new ImgProcessor();
-const blogArticlesShareUrl = import.meta.env.VITE_BLOG_ARTICLES_SHARE_URL;
+// const blogArticlesShareUrl = import.meta.env.VITE_BLOG_ARTICLES_SHARE_URL;
 
 usePageHead("article", {
-  title: "Blog Article",
+  title: article.value.title,
   author: "Maxdlr",
   description: article.value.title,
   image: "/photo.jpg",
@@ -28,58 +28,35 @@ usePageHead("article", {
 onBeforeMount(() => (id.value = router.currentRoute.value.params.id as string));
 onMounted(async () => {
   if (!id.value) return;
+  isLoading.value = true;
   await getArticle();
   await styleArticle();
+  isLoading.value = false;
 });
 const getArticle = async () => {
-  isLoading.value = true;
   article.value = await BlogService.getArticleInfo(id.value as string);
-  isLoading.value = false;
 };
 
 const styleArticle = async () => {
-  article.value.text = marked(article.value.text) as string;
-  article.value.text = article.value.text.replace(
-    /<a href/g,
-    "<a target='_blank' href",
-  );
-
-  article.value.text = Utils.updateDocsAnchorLinks(article.value.text);
-
-  const rules: { [key: string]: string } = {
-    h1: "uk-h1 pt-10",
-    h2: "uk-h2 pt-8",
-    h3: "uk-h3 pt-5",
-    p: "uk-paragraph mt-2",
-    ul: "uk-list uk-list-bullet",
-    img: "rounded-2xl h-[300px] w-[300px] mx-auto",
-    a: "uk-link hover:text-gray-400",
-  };
-  for (const key in rules) {
-    article.value.text = Utils.styleHtml(article.value.text, key, rules[key]);
-  }
-
-  article.value.text = article.value.text.replace(/\\n/g, "<br/>");
-  article.value.text = article.value.text.replace(/\\/g, "");
+  article.value.text = new BlogArticleTextProcessor(
+    article.value.text,
+  ).process();
   article.value.text = await imageProcessor.processText(article.value.text);
-  article.value.text = article.value.text.replace(
-    /<hr>/g,
-    `
-<div class='uk-divider-icon text-center'>
-  <img
-    class="uk-icon-image w-7 h-7 m-auto"
-    src="/logo.png"
-    alt="Maxime de la Rocheterie - FullStack Developer logo"
-  />
-</div>
-`,
-  );
 };
 </script>
 
 <template>
-  <Loader v-if="isLoading" />
-  <section v-else class="uk-container max-w-[800px]">
-    <div v-html="article.text" />
-  </section>
+  <TransitionSlideUp>
+    <div
+      class="w-full h-[40vh] flex justify-center items-center"
+      v-if="isLoading"
+    >
+      <Loader />
+    </div>
+    <main v-else>
+      <article class="uk-container max-w-[800px]">
+        <div v-html="article.text" />
+      </article>
+    </main>
+  </TransitionSlideUp>
 </template>
--- src/composables/Utils.ts
+++ src/composables/Utils.ts
@@ -1,39 +1,6 @@
 const getReadTime = (text: string, wordsPerMinute: number = 200): number =>
   text.trim().split(/\s+/).length / wordsPerMinute;
 
-const styleHtml = (
-  text: string,
-  htmlTagTarget: string,
-  classesToAdd: string,
-) => {
-  const tagRegex = new RegExp(`<${htmlTagTarget}([^>]*)>`, "g");
-
-  return text.replace(tagRegex, (match, attributes) => {
-    if (attributes.includes('class="')) {
-      return match.replace('class="', `class="${classesToAdd} `);
-    } else if (attributes.trim()) {
-      return match.replace(
-        `<${htmlTagTarget}`,
-        `<${htmlTagTarget} class="${classesToAdd}"`,
-      );
-    } else {
-      return `<${htmlTagTarget} class="${classesToAdd}">`;
-    }
-  });
-};
-
-const updateDocsAnchorLinks = (html: string): string => {
-  return html.replace(
-    /<a\s+[^>]*href="(https:\/\/docs\.maxdlr\.com\/doc[^"#]*#h-[^"]*)"[^>]*>(.*?)<\/a>/g,
-    (match, href) => {
-      const newHref = href.split("#")[1];
-      return match.replace(href, "#" + newHref).replace("target='_blank'", "");
-    },
-  );
-};
-
 export const Utils = {
   getReadTime,
-  styleHtml,
-  updateDocsAnchorLinks,
 };
