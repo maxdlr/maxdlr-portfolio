65c25b243e6a9f976df0517f7988b3069e3ca762
maxdlr <contact@maxdlr.com>
2025-06-02 22:15:15 +0200

    rnd boxes

---
+++ src/components/pages/New/Box.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+
+defineProps({
+  w: {
+    type: {} as PropType<
+      | {
+          sm?: number;
+          md?: number;
+          lg?: number;
+          xl?: number;
+          xxl?: number;
+        }
+      | number
+    >,
+  },
+  h: {
+    type: {} as PropType<
+      | {
+          sm?: number;
+          md?: number;
+          lg?: number;
+          xl?: number;
+          xxl?: number;
+        }
+      | number
+    >,
+  },
+  bgColor: { type: String, default: "gray" },
+  bgColorIntensity: { type: Number, default: 200 },
+});
+</script>
+<template>
+  <div
+    :class="[
+      `bg-${bgColor}-${bgColorIntensity}`,
+      w && (typeof w === 'number' || typeof w === 'string')
+        ? 'col-span-' + w
+        : [
+            w && typeof w !== 'number' && w.sm ? 'sm:col-span-' + w.sm : '',
+            w && typeof w !== 'number' && w.md ? 'md:col-span-' + w.md : '',
+            w && typeof w !== 'number' && w.lg ? 'lg:col-span-' + w.lg : '',
+            w && typeof w !== 'number' && w.xl ? 'xl:col-span-' + w.xl : '',
+            w && typeof w !== 'number' && w.xxl ? '2xl:col-span-' + w.xxl : '',
+          ],
+      h && (typeof h === 'number' || typeof h === 'string')
+        ? 'row-span-' + h
+        : [
+            h && typeof h !== 'number' && h.sm ? 'sm:row-span-' + h.sm : '',
+            h && typeof h !== 'number' && h.md ? 'md:row-span-' + h.md : '',
+            h && typeof h !== 'number' && h.lg ? 'lg:row-span-' + h.lg : '',
+            h && typeof h !== 'number' && h.xl ? 'xl:row-span-' + h.xl : '',
+            h && typeof h !== 'number' && h.xxl ? '2xl:row-span-' + h.xxl : '',
+          ],
+    ]"
+    class="rounded-2xl p-3"
+  >
+    <slot> fill me </slot>
+  </div>
+</template>
---
+++ src/components/pages/New/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Box from "./Box.vue";
+</script>
+<template>
+  <section class="grid grid-cols-3 grid-rows-5 gap-3 grid-flow-dense">
+    <Box :w="3">
+      <span>My name is</span>
+    </Box>
+    <Box :w="2" :h="2">
+      <span
+        >text text text text text text text text text text text text text text
+        text text text text text text text text text text text text text text
+        text text text text text text text text text text text text text text
+        text text</span
+      >
+    </Box>
+    <Box :w="1" :h="1">
+      <span>link </span>
+    </Box>
+  </section>
+</template>
